ab028ab4e704fc7941f836501b23df79e2469f60
Tigloo17 <collajm@hotmail.com>
2022-07-25 12:32:56 +0200

    modification pour ajouter le nouveau cli

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@
 $directory = dirname(__DIR__);
 require $directory . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
-$app = new \Tigloo\Base\Application($directory);
+$app = new \Tigloo\Core\Application($directory, false);
 $app->run();
