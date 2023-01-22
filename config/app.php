61aec7c488845349388c9e46cd80f1fea83dedcb
tigloo17 <collajm@hotmail.com>
2023-01-22 10:49:30 +0100

    nouveau provider pour les mail

--- config/app.php
+++ config/app.php
@@ -5,6 +5,7 @@ return [
     "providers" => [
         \Tigloo\Provider\HttpServiceProvider::class,
         \Tigloo\Provider\TwigServiceProvider::class,
-        \Tigloo\Provider\DatabaseServiceProvider::class
+        \Tigloo\Provider\DatabaseServiceProvider::class,
+        \Tigloo\Provider\MailServiceProvider::class,
     ]
 ];
